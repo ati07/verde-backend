4a2feeec6d4fb0bf08226cf41db75e2e15eeb1ed
ati07 <atiurrahman.ansari@gmail.com>
2023-09-10 04:15:48 +0530

    update the user API

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,11 @@
 import User from '../models/user.js';
 import tryCatch from './utils/tryCatch.js';
-
+import bcrypt from 'bcryptjs';
 
 export const addUser= tryCatch(async (req, res) => {
   let userClient = req.body
+  const hashedPassword = await bcrypt.hash(userClient.password, 12);
+  userClient.password = hashedPassword
   const newUser = new User(userClient);
   await newUser.save()
   res.status(200).json({ success: true, message: 'User added successfully' });
@@ -22,16 +24,20 @@ export const deleteUser= tryCatch(async (req, res) => {
 });
 
 export const editUserDetails = tryCatch(async (req, res) => {
+ let findUser= {_id : req.params.userId}
  
-  await User.updateOne(
-    {_id : req.params.userId}, 
-    { $set : req.body }
-    );
+ if(req.body.password){
+  req.body.password = await bcrypt.hash(req.body.password, 12);
+ }
+ let updateUser =  { $set : req.body }
+  
+  await User.updateOne(findUser, updateUser);
+   
   let message = 'User edited successfully'
   if(req.body.isActive  || !req.body.isActive){
     message = 'User status update successfully'
   }else if(req.body.isBlock || !req.body.isBlock){
-    message = 'User status update successfully'
+    message = `User ${req.body.isBlock ? 'Block' :'Unblock'} update successfully`
   }
   res.status(200).json({ success: true, message: message  });
 });
