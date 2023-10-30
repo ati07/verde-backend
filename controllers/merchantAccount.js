c235cac6478e1d91812ab3f66ac5fdbb9c449a28
ati07 <atiurrahman.ansari@gmail.com>
2023-10-30 12:04:20 +0530

    update Merchant A

--- controllers/merchantAccount.js
+++ controllers/merchantAccount.js
@@ -6,9 +6,9 @@ import Chargebacks from '../models/chargebacks.js';
 import Users from '../models/user.js';
 
 export const createMerchantAccount = tryCatch(async (req, res) => {
-  //todo: error handle 
-  let { clientId, merchantId, dba, mcc,midStatus, midLive, mid, rdrActivation, ethocaActivation, } = req.body
-  let merchantAccountPayload = { clientId, merchantId, dba, mcc,midStatus, midLive, mid,  rdrActivation,ethocaActivation }
+  //todo: error handle domainWebPagerdrStatus, ethocaStatus,domainWebPage
+  let { clientId, merchantId, dba, mcc, mid, rdrActivation, ethocaActivation, } = req.body
+  let merchantAccountPayload = { clientId, merchantId, dba, mcc, mid,  rdrActivation,ethocaActivation,domainWebPage }
   if (rdrActivation === 'Yes') {
     if (req.body.rdrCAID) {
       merchantAccountPayload['rdrCAID'] = req.body.rdrCAID
