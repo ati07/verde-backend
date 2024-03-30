28ca871151921a856000b1f3d1c4af9fe416f112
ati07 <atiurrahman.ansari@gmail.com>
2024-03-30 20:40:51 +0530

    add...

---
+++ controllers/crmObservation.js
@@ -0,0 +1,24 @@
+import CrmObservation from "../models/crmObservation.js";
+import tryCatch from "./utils/tryCatch.js";
+
+
+
+export const createCrmObservation = tryCatch(async (req, res) => {
+    let crmObservationPayload = req.body
+    crmObservationPayload.addedBy = req.auth.user._id
+  
+    const newCrmObservation = new CrmObservation(crmObservationPayload);
+  
+    await newCrmObservation.save()
+    res.status(200).json({ success: true, message: 'Observation added successfully' });
+  })
+
+  export const getCrmObservation = tryCatch(async (req, res) => {
+    let findData = {
+        isDelete: false,
+        leadId: req.params.leadId
+      }
+      const crmObservation = await CrmObservation.find(findData).sort({ _id: -1 });
+    
+      res.status(200).json({ success: true, result: crmObservation });
+  })
--- routes/crm.js
+++ routes/crm.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { createCrm, deleteCrm, getCrm, updateCrm } from "../controllers/crm.js";
 import auth from "../middleware/auth.js";
+import { createCrmObservation,getCrmObservation } from "../controllers/crmObservation.js";
 
 
 
 const crmRouter = Router();
 
 crmRouter.post('/', auth, createCrm);
+crmRouter.post('/observation', auth, createCrmObservation);
+crmRouter.get('/observation/:leadId', auth, getCrmObservation);
 crmRouter.get('/', auth, getCrm);
 crmRouter.patch('/:crmId', auth,deleteCrm);
 crmRouter.put('/:crmId', auth, updateCrm);
